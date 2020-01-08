6ef1b49ea43abb6cb12bfdceceb4caa6e3675d9d
Zetsumi <cyril.louisin@outlook.fr>
2020-01-08 22:06:25 +0100

    initial project

---
+++ source/pch.h
@@ -0,0 +1,17 @@
+// pch.h : Il s'agit d'un fichier d'en-tête précompilé.
+// Les fichiers listés ci-dessous sont compilés une seule fois, ce qui améliore les performances de génération des futures builds.
+// Cela affecte également les performances d'IntelliSense, notamment la complétion du code et de nombreuses fonctionnalités de navigation du code.
+// Toutefois, les fichiers listés ici sont TOUS recompilés si l'un d'entre eux est mis à jour entre les builds.
+// N'ajoutez pas de fichiers fréquemment mis à jour ici, car cela annule les gains de performance.
+
+#ifndef PCH_H
+#define PCH_H
+
+#define WIN32_LEAN_AND_MEAN             // Exclure les en-têtes Windows rarement utilisés
+#include <windows.h>
+
+#include "third_party/json/picojson.h"
+#include "third_party/xml/pugixml.h"
+
+
+#endif //PCH_H
